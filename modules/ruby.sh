db72f49b19b8104b3da97abcaa5a0de332afb632
Roman Gonzalez <romanandreg@gmail.com>
2011-04-11 10:26:50 -0700

    Doing some small fixes to the ruby module

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -3,13 +3,16 @@
 # If GEM_HOME has length eq to zero
 # we start with initialization, otherwise
 # we skip
-if [ -z $GEM_HOME ] 
+if [ -z $RUBY_INITIALIZED ] 
 then
-  GEM_HOME="~/.gem"
-  export GEM_HOME
+  RUBY_INITIALIZED='1'
+  if [ -z $GEM_HOME ]
+  then export GEM_HOME="$HOME/.gem"
+  else export GEM_HOME="$GEM_HOME:$HOME/.gem"
+  fi
 
   # Adding Rubygems executables to the PATH
-  PATH="$PATH:~/.gem/bin"
-  export PATH
+  export PATH="$PATH:$HOME/.gem/bin"
+
 fi
 
