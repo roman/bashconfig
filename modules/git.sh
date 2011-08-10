bdddaca8e1bfe41f154b6fc77f8b40fedb4231e4
Roman Gonzalez <romanandreg@gmail.com>
2011-08-10 10:51:40 -0700

    Looking for the vim editor on the /usr/local/bin
    I want the default to be vim, sometimes the installation is on
    /usr/local/bin (if you install from source) so we are going
    to add that path just in case.

--- modules/git.sh
+++ modules/git.sh
@@ -1,13 +1,19 @@
 #!/usr/bin
 
 if [ -f "/usr/bin/vim" ]
-then 
-  # Whe using only the "vim" command (without prefix path), 
-  # the exit status would always be different from zero, 
+then
+  # Whe using only the "vim" command (without prefix path),
+  # the exit status would always be different from zero,
   # by using the complete path, this problem is fixed!
   export GIT_EDITOR="/usr/bin/vim"
+
+elif [ -f "/usr/local/bin/vim" ]
+then
+  export GIT_EDITOR="/usr/local/bin/vim"
+
 # Likely to never be executed :-)
 elif [ -f "/usr/bin/mate" ]
 then
   export GIT_EDITOR="/usr/bin/mate -f"
+
 fi
