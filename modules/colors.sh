463980702fc481431a24d57a49b9e72215c78d71
Roman Gonzalez <romanandreg@gmail.com>
2013-04-05 10:27:09 -0700

    Add magenta foreground color

--- modules/colors.sh
+++ modules/colors.sh
@@ -18,5 +18,6 @@ export LIGHT_RED_FG="\033[1;31m"
 export LIGHT_GREEN_FG="\033[1;32m"
 export LIGHT_BLACK_BG="\033[1;40m"
 export LIGHT_YELLOW_FG="\033[1;33m"
+export LIGHT_MAGENTA_FG="\033[1;35m"
 export RESET="\033[0m"
 
