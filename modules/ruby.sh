645d45aa21dbe22005b5684d7ab2fa067be22107
Roman Gonzalez <romanandreg@gmail.com>
2014-05-28 15:40:11 -0700

    Remove GEM_HOME envar from ruby.sh to avoid conflicts with rvm

--- modules/ruby.sh
+++ modules/ruby.sh
@@ -5,12 +5,12 @@
 # we skip
 if [ -z $RUBY_INITIALIZED ]
 then
-  RUBY_INITIALIZED='1'
-  if [ -z $GEM_HOME ]; then
-      export GEM_HOME="$HOME/.gem"
-  else
-      export GEM_HOME="$GEM_HOME:$HOME/.gem"
-  fi
+  export RUBY_INITIALIZED='1'
+  #if [ -z $GEM_HOME ]; then
+  #    export GEM_HOME="$HOME/.gem"
+  #else
+  #    export GEM_HOME="$GEM_HOME:$HOME/.gem"
+  #fi
 
   # Adding Rubygems executables to the PATH
   if ! [[ $PATH = *$HOME/.gem/bin* ]]; then
