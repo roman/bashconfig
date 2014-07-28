4fc25d094edae8c33eae9802bdde3f98b8162e14
Roman Gonzalez <romanandreg@gmail.com>
2014-07-28 11:38:04 -0700

    Make google-cloud module a bit more smarter

--- modules/google-cloud.sh
+++ modules/google-cloud.sh
@@ -2,9 +2,10 @@
 
 if [[ -z $GOOGLE_CLOUD_SDK_HOME ]]; then
   export GOOGLE_CLOUD_SDK_HOME=$HOME/Library/google-cloud-sdk
-  # The next line updates PATH for the Google Cloud SDK.
-  source $GOOGLE_CLOUD_SDK_HOME/path.bash.inc
-
-  # The next line enables bash completion for gcloud.
-  source $GOOGLE_CLOUD_SDK_HOME/completion.bash.inc
+  if [[ -d $GOOGLE_CLOUD_SDK_HOME ]]; then
+    # The next line updates PATH for the Google Cloud SDK.
+    source $GOOGLE_CLOUD_SDK_HOME/path.bash.inc
+    # The next line enables bash completion for gcloud.
+    source $GOOGLE_CLOUD_SDK_HOME/completion.bash.inc
+  fi
 fi
