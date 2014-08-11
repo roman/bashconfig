231de15e8d63a93f14e61c48747d47a81064c4e4
Roman Gonzalez <romanandreg@gmail.com>
2014-08-11 09:29:21 -0700

    Perform rvm actions only if rvm is installed

--- modules/rvm.sh
+++ modules/rvm.sh
@@ -1,5 +1,5 @@
 # This loads the rvm configurations
-if ! [[ $PATH = *$HOME/.rvm/bin* ]]; then
+if [[ -d $HOME/.rvm ]] && ! [[ $PATH = *$HOME/.rvm/bin* ]]; then
     export PATH="$HOME/.rvm/bin:$PATH"
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 fi
