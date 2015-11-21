84c2adfc513da0c949d8822f6dd33728d56a017f
Roman Gonzalez <romanandreg@gmail.com>
2015-11-20 21:41:13 -0800

    typo fix

--- modules/localbin.sh
+++ modules/localbin.sh
@@ -3,5 +3,5 @@
 # If the home binary folder is not included
 # in the PATH variable, include it.
 if ! [[ $PATH = *$HOME/.bin* ]]; then
-  export PATH="$HOME/local/.bin:$PATH"
+  export PATH="$HOME/.local/bin:$PATH"
 fi
